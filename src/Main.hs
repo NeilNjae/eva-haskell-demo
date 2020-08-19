dedd20b7ef05dc53b0935bdfc5daf582f8111bad
Neil Smith <neil.git@njae.me.uk>
2020-08-19 11:48:31 +0100

    Tidying

--- src/Main.hs
+++ src/Main.hs
@@ -19,18 +19,16 @@ data Tree a = Branch a (Tree a) (Tree a)
 instance Functor Tree where
     fmap = mapTree
 
-
-instance Foldable Tree where
-    foldMap f (Leaf x) = f x
-    foldMap f (Branch x l r) = (foldMap f l) <> (f x) <> (foldMap f r) 
-
-
 mapTree :: (a -> b) -> Tree a -> Tree b
 mapTree f (Leaf x) = Leaf (f x)
 mapTree f (Branch x left right) = 
     Branch (f x) 
            (mapTree f left)
            (mapTree f right)
+
+instance Foldable Tree where
+    foldMap f (Leaf x) = f x
+    foldMap f (Branch x l r) = (foldMap f l) <> (f x) <> (foldMap f r) 
 
 
 
@@ -67,7 +65,7 @@ intTree =
         )
 
 
-ppt :: (Show a) => (Tree a) -> (IO ())
+ppt :: (Show a) => Tree a -> IO ()
 ppt = putStrLn . DT.drawTree . toDataTree
 
 
@@ -78,49 +76,6 @@ main = do
   ppt intTree
 
 
-fibNaive :: Int -> Int
-fibNaive 0 = 0
-fibNaive 1 = 1
-fibNaive n = fibNaive (n - 1) + fibNaive (n - 2)
-
-fibsDirect = fibsDirectHelper 0
-    where fibsDirectHelper n = (fibNaive n) : (fibsDirectHelper (n + 1))
-
-
-fibsNaive = map fibNaive [0..]
-
-
--- 0  1  1  2  3  5  8 13 21 34  fibs
--- 1  1  2  3  5  8 13 21 34     tail fibs
--- 1  2  3  5  8 13 21 34        zipWith (+)
-
-
-fibs :: [Int]
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
--- fib :: Int -> Int
--- fib n = genericIndex fibs n
-fib n = fibs!!n
-
-
-sumFirstFibs howMany = sumList nums
-    where nums = take howMany fibs
-          sumList [] = 0
-          sumList (n:ns) = n + sumList ns
-
-
-fibsU :: [Int]
-fibsU = unfoldr (\(a, b) -> Just (a, (b, a+b))) (0, 1)
-
-fibsULimit :: Int -> [Int]
-fibsULimit limit = 
-    unfoldr 
-        (\(a, b) -> if a > limit 
-                    then Nothing 
-                    else Just (a, (b, a+b))) 
-        (0, 1)
-
-
 names :: [String]
 names = ["alice", "bob", "eve"]
 
@@ -128,60 +83,7 @@ suffs :: [String]
 suffs =  ["!", "?", " :-)"]
 
 
-
-type Name = String
-type Key = String
-type Address = String
-type Cost = Int
+fibsU :: [Int]
+fibsU = unfoldr (\(a, b) -> Just (a, (b, a+b))) (0, 1)
 
 
-
-keyFromName :: Name -> Key
-keyFromName name = name ++ "'s key"
-
-addressFromKey :: Key -> Address
-addressFromKey key = key ++ "'s address"
-
-postCostFromAddress :: Address -> Cost
-postCostFromAddress address = length address
-
-addressFromName = addressFromKey . keyFromName
-
-postCostFromName = postCostFromAddress . addressFromKey . keyFromName
-
-
-maybeKeyFromName :: Name -> Maybe Key
-maybeKeyFromName name 
-    | name == "neil" = Just (name ++ "'s key")
-    | otherwise      = Nothing
-
-maybeAddressFromKey :: Key -> Maybe Address
-maybeAddressFromKey key = Just (key ++ "'s address")
-
-maybePostCostFromAddress :: Address -> Maybe Cost
-maybePostCostFromAddress address = Just (length address)
-
-
--- maybeAddressFromName name = 
---     case (maybeKeyFromName name) of
---         Just key -> maybeAddressFromKey key
---         Nothing -> Nothing
-
--- maybePostCostFromName name = 
---     case (maybeKeyFromName name) of
---         Just key -> case (maybeAddressFromKey key) of
---                         Just address -> maybePostCostFromAddress address
---                         Nothing -> Nothing
---         Nothing -> Nothing
-
-
-composeMaybe :: (b -> Maybe c) -> (a -> Maybe b) -> (a -> Maybe c)
-composeMaybe f g = \x -> case (g x) of
-                            Just y -> f y
-                            Nothing -> Nothing
-
-maybePostCostFromName = maybePostCostFromAddress `composeMaybe` maybeAddressFromKey `composeMaybe` maybeKeyFromName
-
-(<=<) = composeMaybe
-
-mpcfn = maybePostCostFromAddress <=< maybeAddressFromKey <=< maybeKeyFromName
